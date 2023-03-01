94a0f0772fa6d6a8de6f4aa5fc70d54ea2e83141
Bruno-Morales <br1_morales@hotmail.com>
2023-03-01 01:49:40 -0300

    Segundo entregable en proceso

--- Primer-Entregable.js
+++ Primer-Entregable.js
@@ -1,11 +1,23 @@
+const fs = require("fs");
+
 class ProductManager {
   constructor() {
-    this.products = [];
+    this.path = "./files/Products.json";
   }
 
-  addProduct = (title, description, price, thumbnail, code, stock) => {
-    const coderepit = this.products.find((event) => event.code === code);
+  getProducts = async () => {
+    if (fs.existsSync(this.path)) {
+      const data = await fs.promises.readFile(this.path, "utf-8");
+      const result = JSON.parse(data);
+      return result;
+    } else {
+      return [];
+    }
+  };
 
+  addProduct = async (title, description, price, thumbnail, code, stock) => {
+    const products = await this.getProducts();
+    const coderepit = products.find((product) => product.code === code);
     if (coderepit) {
       console.log("This Code does exist!");
       return;
@@ -14,9 +26,8 @@ class ProductManager {
       console.log("You must complete all fields");
       return;
     }
-
-    const event = {
-      id: this.products.length + 1,
+    const product = {
+      id: products.length + 1,
       title,
       description,
       price,
@@ -25,69 +36,131 @@ class ProductManager {
       stock,
     };
 
-    this.products.push(event);
+    products.push(product);
+
+    await fs.promises.writeFile(
+      this.path,
+      JSON.stringify(products, null, "\t")
+    );
+    return product;
   };
 
-  getProductById = (id) => {
-    const event = this.products.find((event) => event.id === id);
-
-    if (!event) {
-      console.log("Product not found");
-      return;
+  getProductById = async (id) => {
+    if (fs.existsSync(this.path)) {
+      const data = await fs.promises.readFile(this.path, "utf-8");
+      const result = JSON.parse(data);
+      const event = result.find((product) => product.id === id);
+      if (!event) {
+        console.log("Product not found");
+        return;
+      }
+      return event;
     }
-    return event;
   };
 
-  getElements = () => {
-    return this.products;
+  updateProduct = async (id, campo, newDate) => {
+    if (fs.existsSync(this.path)) {
+      const data = await fs.promises.readFile(this.path, "utf-8");
+      const result = JSON.parse(data);
+      const event = result.find((product) => product.id === id);
+      if (!event) {
+        console.log("Product not found");
+        return;
+      }
+      let llaveValor = Object.entries(event);
+
+      let soloValores = Object.values(event);
+
+      if (llaveValor[1][0] === campo) {
+        event.title = newDate;
+        console.log(event);
+      }
+      if (llaveValor[2][0] === campo) {
+        event.description = newDate;
+        console.log(event);
+      }
+      if (llaveValor[3][0] === campo) {
+        event.price = newDate;
+      }
+      if (llaveValor[4][0] === campo) {
+        event.thumbnail = newDate;
+        console.log(event);
+      }
+      if (llaveValor[5][0] === campo) {
+        event.code = newDate;
+        console.log(event);
+      }
+      if (llaveValor[6][0] === campo) {
+        event.stock = newDate;
+        console.log(event);
+      }
+    }
   };
+
+  // updateProduct = async (id, campo, newDate) => {
+  //   if (fs.existsSync(this.path)) {
+  //     const data = await fs.promises.readFile(this.path, "utf-8");
+  //     const result = JSON.parse(data);
+  //     const event = result.find((product) => product.id === id);
+  //     if (!event) {
+  //       console.log("Product not found");
+  //       return;
+  //     }
+  //     let llaveValor = Object.entries(event);
+  //     let soloValores = Object.values(event);
+
+  //     let productNew = {
+  //       id: event.id,
+  //       title: event.title,
+  //       description: event.description,
+  //       price: event.price,
+  //       thumbnail: event.thumbnail,
+  //       code: event.code,
+  //       stock: event.stock,
+  //     };
+
+  //     for (let i = 0; i < llaveValor.length; i++) {
+  //       if (llaveValor[i][0] === campo) {
+  //         llaveValor[i][0] = campo;
+  //         //aca pruebo agregandolo pero se agrega al final de todo
+  //         //await fs.promises.appendFile(this.path, newDate);
+  //         //aca pruebo asignandole el nuevo valor, pero no se retorna.
+  //         event.campo = newDate;
+  //         console.log(event);
+  //       }
+  //     }
+  //   }
+  // };
 }
 
 const productManager = new ProductManager();
 
-productManager.addProduct(
-  "Coca cola",
-  "Coca cola de 1 Litro retornable",
-  250,
-  "https://upload.wikimedia.org/wikipedia/commons/e/e8/15-09-26-RalfR-WLC-0098_-_Coca-Cola_glass_bottle_%28Germany%29.jpg",
-  9,
-  100
-);
-productManager.addProduct(
-  "Sprite",
-  "Sprite retornable",
-  350,
-  "https://upload.wikimedia.org/wikipedia/commons/e/e8/15-09-26-RalfR-WLC-0098_-_Coca-Cola_glass_bottle_%28Germany%29.jpg",
-  10,
-  800
-);
+const consulta = async () => {
+  let primeraConsulta = await productManager.getProducts();
+  console.log("------------------");
+  console.log(primeraConsulta + "     Esta consulta primero sale vacía.");
+  console.log("------------------");
+  await productManager.addProduct(
+    "Primer Producto Manaos",
+    "Prueba con manaos 2 litros",
+    200,
+    "https://google.com",
+    22,
+    130
+  );
+  console.log("------------------");
+  console.log("--Consulta con producto por ID----");
+  console.log("------------------");
 
-productManager.addProduct(
-  "Manaos",
-  "Manaos de 1 Litro retornable",
-  150,
-  "https://upload.wikimedia.org/wikipedia/commons/e/e8/15-09-26-RalfR-WLC-0098_-_Coca-Cola_glass_bottle_%28Germany%29.jpg",
-  11,
-  80
-);
+  console.log(await productManager.getProductById(1));
 
-//Prueba de productos,
+  console.log("------------------");
+  console.log("--Consulta con producto modificado----");
+  console.log("------------------");
 
-console.log(productManager.products);
+  //para modificar poroducto, agregar primer parametro id, segundo el campo a modificar, y tercero su valor.
 
-// SI se agrega Otro producto con el Mismo codigo. Arroja primero el mensaje "This Code does exist!".
+  await productManager.updateProduct(1, "code", 160);
+};
 
-//productManager.addProduct(
-// "Manaos",
-// "Manaos de 1 Litro retornable",
-//  150,
-//  "https://upload.wikimedia.org/wikipedia/commons/e/e8/15-09-26-RalfR-WLC-0098_-_Coca-Cola_glass_bottle_%28Germany%29.jpg",
-//  11,
-//  80
-//);
-
-// console.log(productManager.products);
-
-//Prueba de productos por ID
-
-productManager.getProductById(2);
+consulta();
